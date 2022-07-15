1caddfb4841977c892d6fd9b16311c8f2c042d2b
candicejwy <44786518+candicejwy@users.noreply.github.com>
2022-07-15 15:08:10 -0400

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+class Database:
+    def __init__(self,db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.conn.commit()
+      
+
+    def insert(self,title, author, year, isbn):
+        self.cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author, year, isbn))
+        self.conn.commit()
+        
+
+
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+
+    def search(self,title="",author="",year="",isbn=""):
+        
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author =? OR year=? OR isbn=?",(title,author, year, isbn))
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+
+    def delete(self,id):
+        self.cur.execute("DELETE  FROM book WHERE id=? ",(id,))
+        self.conn.commit()
+        
+
+    def update(self,id,title, author, year, isbn):
+        
+        self.cur.execute("UPDATE book SET title=?,author=?,year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+        self.conn.commit()
+        
+    def __del__(self):
+        self.conn.close()
+# insert("The Sun","John Smith", 1918, 1881233)
+# delete(4)
+# update(3,"The Earth","John Smith",1918,4198888)
+# print(view())
+# print(search(author="JOHN SMITH"))
